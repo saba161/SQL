33964292e5cf00d49aa12db7ccac56c75d419853
Macbook <macbook@192.168.1.33>
2019-01-10 00:27:15 +0400

    base

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -5,7 +5,15 @@ CREATE TABLE Cars(
     Model INT
 )
 
+CREATE TABLE Bus(
+    Name NVARCHAR(50),
+    Model INT
+)
+INSERT INTO Bus(Name, Model)
+VALUES ('MAN', 599)
+
 INSERT INTO Cars(Name, Model)
 VALUES ('BMW', 535), ('Merz', 190), ('BMW', 535)
 
 SELECT * FROM Cars
+SELECT * FROM Bus
