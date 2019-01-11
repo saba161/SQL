3e592761d31f42a09f1c1da9215c204a285d8adb
Macbook <macbook@192.168.1.33>
2019-01-11 18:55:26 +0400

    base

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -11,7 +11,4 @@ City varchar(255)
 INSERT INTO Persons(PersonID, FirstName, LastName, Address, City)
 VALUES (1, 'saba', 'koghuashvili', 'banoja', 'Kutaisi')
 
-INSERT INTO Cars(Name, Model)
-VALUES ('BMW', 535), ('Merz', 190), ('BMW', 535)
-
 SELECT * FROM Persons
