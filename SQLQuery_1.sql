869de40e3b500370275da0b58e38173402330847
Macbook <macbook@192.168.1.33>
2019-01-11 18:54:19 +0400

    base

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,19 +1,17 @@
 CREATE DATABASE Saba;
 
-CREATE TABLE Cars(
-    Name NVARCHAR(50),
-    Model INT
+CREATE TABLE Persons(
+PersonID int,
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255)
 )
 
-CREATE TABLE Bus(
-    Name NVARCHAR(50),
-    Model INT
-)
-INSERT INTO Bus(Name, Model)
-VALUES ('MAN', 599)
+INSERT INTO Persons(PersonID, FirstName, LastName, Address, City)
+VALUES (1, 'saba', 'koghuashvili', 'banoja', 'Kutaisi')
 
 INSERT INTO Cars(Name, Model)
 VALUES ('BMW', 535), ('Merz', 190), ('BMW', 535)
 
-SELECT * FROM Cars
-SELECT * FROM Bus
+SELECT * FROM Persons
