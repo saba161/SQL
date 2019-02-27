c9c421a20ecfbf9b6193d7af72d574481cc4ce9d
kogo <saba.koguashvili1999@mail.ru>
2019-02-27 22:18:09 +0400

    sql

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -9,16 +9,34 @@ CREATE TABLE Student(
 )
 
 INSERT INTO Student(PersonID, FirstName, LastName, Country, City)
-VALUES(3, 'Saba', 'Koghuashvili', 'Georgia', 'Kutaisi')
+VALUES(3, 'Saba', 'Koghuashvili', 'Georgia', 'Kutaisi'),
+(4, 'Saba', 'Koghuashvili', 'Georgia', 'Kutaisi'),
+(5, 'lado', 'Kogli', 'Georgia', 'tbilisi'),
+(6, 'jano', 'irema', 'Georgia', 'batumi'),
+(7, 'nunu', 'loer', 'Georgia', 'xoni')
+
 
 ALTER TABLE Student
 ADD CONSTRAINT UC_Student UNIQUE (PersonID)
 
 SELECT * FROM Student;
-DROP TABLE Student;
+DROP TABLE Blog;
 
 ALTER TABLE Student
 ADD UserID int IDENTITY(1,1)
 
 ALTER TABLE Student
 ADD PRIMARY KEY (UserID);
+
+CREATE TABLE Blog (
+    BlogId int IDENTITY(1,1),
+    Name NVARCHAR(50) NOT NULL,
+    CreateDate DATETIME NOT NULL
+)
+Go
+
+ 
+
+ALTER TABLE Blog
+ADD UserId int NOT NULL
+GO
